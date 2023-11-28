4589307410961d3ca96ff72934462fb4b6b7a69b
Aurore <112873099+AurorePhl@users.noreply.github.com>
2023-11-28 09:24:00 +0100

    Update succès.php

--- succès.php
+++ succès.php
@@ -1,5 +1,6 @@
 <?php 
 // conditions qui redirige vers succès.html si le bon film a été trouvé. 
+header("Location: succès.html");
 
 //condition si ce n'est pas le bon film. 
 echo '<p>Ce n'est pas le film que vous devez trouver.</p>';
