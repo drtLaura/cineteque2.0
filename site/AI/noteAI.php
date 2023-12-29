7ee49a36cc9261cd84e1031d3592448b2878688a
Aurore <112873099+AurorePhl@users.noreply.github.com>
2023-12-29 17:11:57 +0100

    Add files via upload

--- site/AI/noteAI.php
+++ site/AI/noteAI.php
@@ -1,6 +1,23 @@
 <?php
-session_start();
-
+session_start(); // démarrage session
+//Randomiser l'ordre des items du menu de navigation
+$menuLiens=array("1.php", "2.php","3.php", "4.php" , "5.php");
+shuffle($menuLiens);
+$_SESSION["lien1"]=$menuLiens[0];
+$_SESSION["lien2"]=$menuLiens[1];
+$_SESSION["lien3"]=$menuLiens[2];
+$_SESSION["lien4"]=$menuLiens[3];
+$_SESSION["lien5"]=$menuLiens[4];
+$menuItems=array(substr($_SESSION["lien1"],0,strlen($_SESSION['lien1'])-4),
+substr($_SESSION["lien2"],0,strlen($_SESSION['lien2'])-4),
+substr($_SESSION["lien3"],0,strlen($_SESSION['lien3'])-4),
+substr($_SESSION["lien4"],0,strlen($_SESSION['lien4'])-4),
+substr($_SESSION["lien5"],0,strlen($_SESSION['lien5'])-4));
+$_SESSION["item1"]=$menuItems[0];
+$_SESSION["item2"]=$menuItems[1];
+$_SESSION["item3"]=$menuItems[2];
+$_SESSION["item4"]=$menuItems[3];
+$_SESSION["item5"]=$menuItems[4];
 ?>
 
 <!DOCTYPE html>
@@ -8,13 +25,65 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Note</title>
+    <link rel="stylesheet" href="../site.css">
+    <link rel="stylesheet" href="ai.css">
+    <title>Genre</title>
 </head>
 <body>
-    <h1>Note</h1>
-    <p> Voici la consigne que vous devez réaliser : <?php echo $_SESSION['consigne']?> </p>
-    <form action="succesAI.php" method="post">
-        <input type="submit" value="Aller à succès">
+    <header>
+      <div id="en-tete">
+        <div id="logo">
+            <form method="post" action="accueil.php">
+          <button name="logo"><img src="../img/logo.png"/></button>
+                </form>
+        </div>
+        <div id="consigne">
+            <h3>Voici la consigne que vous devez réaliser : <?php echo $_SESSION['consigne'];?></h3>
+        </div>
+        <div id="timer">
+          <p></p>
+        </div>
+      </div>
+      <nav id="menu-genre">
+        <ul>
+            <li>
+                <a href="<?php echo $_SESSION['lien1'];?>"><?php echo $_SESSION['item1'];?></a>
+            </li>
+            <li>
+                <a href="<?php echo $_SESSION['lien2'];?>"><?php echo $_SESSION['item2'];?></a>
+            </li>
+            <li>
+                <a href="<?php echo $_SESSION['lien3'];?>"><?php echo $_SESSION['item3'];?></a>
+            </li>
+            <li>
+                <a href="<?php echo $_SESSION['lien4'];?>"><?php echo $_SESSION['item4'];?></a>
+            </li>
+            <li>
+                <a href="<?php echo $_SESSION['lien5'];?>"><?php echo $_SESSION['item5'];?></a>
+            </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+        <div id="films">
+        <p id="message">Ce n'est pas la bonne page.</p>
+      <ul>
+              
+          <li>
+        <form method="post" action="../../succès.php">              <a>En savoir plus</a>
+</form>
+          </li>
+      </ul>
+            <ul>
+          <li>
+              <form action="paysAI.php" method="post">
+        <input type="submit" value="Aller à Pays">
     </form>
+          </li>
+      </ul>
+            </div>
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
--- site/AI/succesAI.php
+++ site/AI/succesAI.php
@@ -2,57 +2,57 @@
 session_start();
 
 $passetotal = time() - $_SESSION['debut'];
-echo "passetotal :". $passetotal;
+//echo "passetotal :". $passetotal;
 
     if ($_SESSION['indexconsigne'] == 1){
-    echo 'indexconsigne = 1';
+    //echo 'indexconsigne = 1';
     $_SESSION['passecons1'] = $passetotal;
-    echo "passe consigne 1 ". $_SESSION['passecons1'];
+    //echo "passe consigne 1 ". $_SESSION['passecons1'];
     $_SESSION['data'] = array(
         'id' => $_SESSION['id'],
         'temps passe consigne 1' => $_SESSION['passecons1']
     );
 }
     if ($_SESSION['indexconsigne'] ==  2) {
-        echo 'indexconsigne = 2';
+        //echo 'indexconsigne = 2';
         $_SESSION['passecons2'] = $passetotal;
-        echo "passe consigne 2 " . $_SESSION['passecons2'];
+        //echo "passe consigne 2 " . $_SESSION['passecons2'];
         $_SESSION['data'] = array_merge($_SESSION['data'], array(
             'temps passe consigne 2' => $_SESSION['passecons2']
         ));
     }
 
     if ($_SESSION['indexconsigne'] == 3) {
-        echo 'indexconsigne = 3';
+        //echo 'indexconsigne = 3';
         $_SESSION['passecons3'] = $passetotal;
-        echo "passe consigne 3 " . $_SESSION['passecons3'];
+        //echo "passe consigne 3 " . $_SESSION['passecons3'];
         $_SESSION['data'] = array_merge($_SESSION['data'], array(
             'temps passe consigne 3' => $_SESSION['passecons3']
         ));
     }
     if ($_SESSION['indexconsigne'] == 4) {
-        echo 'indexconsigne = 4';
+        //echo 'indexconsigne = 4';
         $_SESSION['passecons4'] = $passetotal;
-        echo "passe consigne 4 " . $_SESSION['passecons4'];
+        //echo "passe consigne 4 " . $_SESSION['passecons4'];
         $_SESSION['data'] = array_merge($_SESSION['data'], array(
             'temps passe consigne 4' => $_SESSION['passecons4']
         ));
     }
     if ($_SESSION['indexconsigne'] == 5) {
-        echo 'indexconsigne = 5';
+        //echo 'indexconsigne = 5';
         $_SESSION['passecons5'] = $passetotal;
-        echo "passe consigne 5 " . $_SESSION['passecons5'];
+        //echo "passe consigne 5 " . $_SESSION['passecons5'];
         $_SESSION['data'] = array_merge($_SESSION['data'], array(
             'temps passe consigne 5' => $_SESSION['passecons5']
         ));
     }
-    echo "data : ";
-    print_r($_SESSION['data']);
-    echo "avant" . $_SESSION['indexconsigne'];
+    //echo "data : ";
+    //print_r($_SESSION['data']);
+    //echo "avant" . $_SESSION['indexconsigne'];
     if (isset($_SESSION['indexconsigne'])) {
         $_SESSION['indexconsigne']++;
     }
-    echo "après" . $_SESSION['indexconsigne'];
+    //echo "après" . $_SESSION['indexconsigne'];
     ?>
 
     <!DOCTYPE html>
@@ -60,6 +60,7 @@ echo "passetotal :". $passetotal;
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../style.css">
         <title>Succès</title>
         
     </head>
@@ -67,10 +68,25 @@ echo "passetotal :". $passetotal;
         
 
         <h1>Succès</h1>
-        <p>Temps passé sur les pages précédentes: <?php  print_r($_SESSION['data']); ?> secondes</p>
+        <?php  
+        echo "<ul>";
+        $length=0;
+        foreach($_SESSION['data'] as $cpt){
+            $length++;
+        }
+        $index=0;
+        for($index;$index<$length;$index++){
+            if($index===0){
+                echo "<li> Identifiant : ". $_SESSION['data']['id']."</li>";
+            }
+            else {
+                echo "<li> Tâche ".$index." : ".$_SESSION['data']['temps passe consigne '.$index]." secondes</li>";
+            }
+        }
+        echo "</ul>";
 
    
-<?php if ($_SESSION['indexconsigne'] <= 5) {
+if ($_SESSION['indexconsigne'] <= 5) {
     $ref = "genreAI.php?id=".$_SESSION['id'];
     $msgbutton = "Continuer";
 }
